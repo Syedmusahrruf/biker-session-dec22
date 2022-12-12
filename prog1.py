4e7363587c074ca78a8b8b7a31d28e9b95e41e24
Syedmusharruf <119780049+Syedmusahrruf@users.noreply.github.com>
2022-12-12 14:12:41 +0530

    Create prog1.py

---
+++ prog1.py
@@ -0,0 +1,15 @@
+def list_occurences(l):
+    return True if l.count(19) == 2 and l.count(5) >=3 else False
+
+print(list_occurences([19, 19, 5, 5, 5, 5, 5]))
+
+
+def list_occurences(l):
+    return True if l.count(19) == 2 and l.count(5) >=3 else False
+
+print(list_occurences([19, 15, 15, 5, 3, 3, 5, 2]))
+
+def list_occurences(l):
+    return True if l.count(19) == 2 and l.count(5) >=3 else False
+
+print(list_occurences([19, 19, 5, 5, 5, 5, 5]))
