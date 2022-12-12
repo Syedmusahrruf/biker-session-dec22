b77a097730875f821bb0c0d9ea11075d3b4a2c38
Syedmusharruf <119780049+Syedmusahrruf@users.noreply.github.com>
2022-12-12 14:25:38 +0530

    Create prgm2.py

---
+++ prgm2.py
@@ -0,0 +1,23 @@
+def test(nums):
+    return len(nums) == 8 and nums.count(nums[4]) == 3
+
+nums = [19, 19, 15, 5, 5, 5, 1, 2]
+print(test(nums))
+
+def test(nums):
+    return len(nums) == 8 and nums.count(nums[4]) == 3
+
+nums = [19, 15, 5, 7, 5, 5, 2]
+print(test(nums))
+
+def test(nums):
+    return len(nums) == 8 and nums.count(nums[4]) == 3
+
+nums = [11, 12, 14, 13, 14, 13, 15, 14]
+print(test(nums))
+
+def test(nums):
+    return len(nums) == 8 and nums.count(nums[4]) == 3
+
+nums = [19, 15, 11, 7, 5, 6, 2]
+print(test(nums))
