85b38628013ae085fd550e21375afad0cc0c1f49
Syedmusharruf <119780049+Syedmusahrruf@users.noreply.github.com>
2022-12-12 14:27:38 +0530

    Create program4.py

---
+++ program4.py
@@ -0,0 +1,19 @@
+def test(n):
+    return [n + 2 * i for i in range(n)]
+n = 10
+print(test(n))
+
+def test(n):
+    return [n + 2 * i for i in range(n)]
+n = 2
+print(test(n))
+
+def test(n):
+    return [n + 2 * i for i in range(n)]
+n = 3
+print(test(n))
+
+def test(n):
+    return [n + 2 * i for i in range(n)]
+n = 17
+print(test(n))
