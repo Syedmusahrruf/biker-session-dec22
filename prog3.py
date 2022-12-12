5246d9761796041df9aa623485fe8cbc4775b6ab
Syedmusharruf <119780049+Syedmusahrruf@users.noreply.github.com>
2022-12-12 14:15:32 +0530

    Create prog3.py

---
+++ prog3.py
@@ -0,0 +1,21 @@
+def test(n):
+    return n % 34 == 4 and n > 4 ** 4
+
+n=922
+print(test(n))
+
+def test(n):
+    return n % 34 == 4 and n >  4 ** 4
+
+n=914
+print(test(n))
+
+def test(n):
+    return n % 34 == 4 and n > 4 ** 4
+n=854
+print(test(n))
+
+def test(n):
+    return n % 34 == 4 and n > 4 ** 4
+n=854
+print(test(n))
