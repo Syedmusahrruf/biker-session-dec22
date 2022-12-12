ee2b86df1340e5166d7f7145c00077b483f74aa9
Syedmusharruf <119780049+Syedmusahrruf@users.noreply.github.com>
2022-12-12 23:42:44 +0530

    Update prgm5.py

---
+++ prgm5.py
@@ -0,0 +1,23 @@
+def test(str1):
+    return str1[len(str1)-2] in str1[len(str1)-1] and str1[len(str1)-2] != str1[len(str1)-1]
+
+str1= ["a","abb","sfs","oo","de","sfde"]
+print(test(str1))
+
+def test(str1):
+    return str1[len(str1)-2] in str1[len(str1)-1] and str1[len(str1)-2] != str1[len(str1)-1]
+
+str1= ["a","abb","sfs","oo","ee","sfde"]
+print(test(str1))
+
+def test(str1):
+    return str1[len(str1)-2] in str1[len(str1)-1] and str1[len(str1)-2] != str1[len(str1)-1]
+
+str1= ["a","abb","sad","ooaaesdfe","sfsdfde","sfsd","sfsdf","qwrew"]
+print(test(str1))
+
+def test(str1):
+    return str1[len(str1)-2] in str1[len(str1)-1] and str1[len(str1)-2] != str1[len(str1)-1]
+
+str1= ["a","abb","sad","ooaaesdfe","sfsdfde","sfsd","sfsdf","qwsfsdfrew"]
+print(test(str1))
